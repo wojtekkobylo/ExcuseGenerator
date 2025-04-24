13f3284838150242c545403c24ff168d86e81b9d
Maks <maksymilian0606@wp.pl>
2025-04-24 16:42:29 +0200

    localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,114 +1,41 @@
 
-import './App.css'
-import {useState} from "react";
-import './Form.css'
 
-interface FormData{
-  imie: string;
-  powod: string;
-  poziomwiary: string;
-  data: string;
-  kreatywnosc: string;
-  szczegoly: string;
-  pilnosc: boolean;
-}
+import Form from "./Form.tsx";
+import {useState} from "react";
+import ListaWymowek from "./ListaWymowek.tsx";
+
+
 
 function App() {
-    let pilnosc: string;
-  const [formData, setFormData] = useState<FormData>({ imie: "", powod: "" ,poziomwiary: "",data: "",kreatywnosc: "",szczegoly: "", pilnosc: false});
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
-    const { name, value, type, checked } = e.target as HTMLInputElement;
-    setFormData((prev) => ({ ...prev, [name]: type==="checkbox"? checked : value }));
-  };
-  if (formData.pilnosc){
-    pilnosc = "wymówke oznaczono jako pilną";
-  }
-  else{
-    pilnosc = "wymówki nie oznaczono jako pilną"
-  }
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log(formData.imie,'\n',formData.powod,'\n',`tresc wymowki: ${formData.szczegoly}`,'\n',`zdarzenie miało miejsce:${formData.data}`,'\n'
-        ,`ta wymówka jest ${formData.kreatywnosc}`,'\n',pilnosc)
-  };
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Imie:
-          <input
-            type="text"
-            name="imie"
-            value={formData.imie}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Powód wymówki:
-          <select
-              name="powod"
-              value={formData.powod}
-              onChange={handleChange}
-          >
-            <option value="">-- Wybierz --</option>
-            <option value="spóźnienie">spóźnienie</option>
-            <option value="niewykonanie zadania">niewykonanie zadania</option>
-            <option value="brak pracy domowej">brak pracy domowej</option>
-          </select>
-        </label>
-        <label>
-          Poziom wiarygodności
-          <input type="range" name="poziomwiary" min="1" max="10" value={formData.poziomwiary}
-                 onChange={handleChange}/>
-        </label>
-        <label>
-          Data zdarzenia:
-          <input
-              type="date"
-              name="data"
-              value={formData.data}
-              onChange={handleChange}
-          />
-        </label>
-        <label>
-          Poziom kreatywności:
-          <select
-              name="kreatywnosc"
-              value={formData.kreatywnosc}
-              onChange={handleChange}
-          >
-            <option value="">-- Wybierz --</option>
-            <option value="mało kreatywna">mało kreatywna</option>
-           <option value="przeciętnie kreatywna">przeciętna</option>
-            <option value="bardzo kreatywna">bardzo kreatywna</option>
-          </select>
-        </label>
-        <label>
-          Dodatkowe szczegóły:
-          <textarea
-              name="szczegoly"
-              value={formData.szczegoly}
-              onChange={handleChange}
-          />
-        </label>
-        <label>
-          Pilna wymówka
-          <input
-              type="checkbox"
-              name="pilnosc"
-              checked={formData.pilnosc}
-              onChange={handleChange}
-          />
+    const [listaWym, setListaWym] = useState<Array<string>>(() => {
+        const savedExcuses = localStorage.getItem("wymowki");
+        return savedExcuses ? JSON.parse(savedExcuses) : [];
+    });
 
-        </label>
-        <button type="submit">Submit</button>
-      </form>
 
-    </>
-  )
+    function onButtonClick2(msg:string) {
+        const updatedList=([...listaWym, msg]);
+        setListaWym(updatedList);
+        localStorage.setItem("wymowki", JSON.stringify(updatedList));
+    }
+
+
+    function clearExcuses() {
+        setListaWym([]);
+        localStorage.removeItem("wymowki");
+    }
+
+
+    return (
+        <>
+            <Form onButtonClick={onButtonClick2}/>
+            <ListaWymowek listaWymowek={listaWym}/>
+            <button onClick={clearExcuses}>Wyczysc Wymowki</button>
+        </>
+    )
 }
 
 export default App
